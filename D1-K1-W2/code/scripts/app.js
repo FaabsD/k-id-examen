580464905d5fb2b6ec8f11caa3bab64a2a3d86dd
Fabian <19959@ma-web.nl>
2021-05-14 18:17:58 +0200

    navigatie uitgewerkt

---
+++ D1-K1-W2/code/scripts/app.js
@@ -0,0 +1,20 @@
+console.log('hallo');
+
+
+if (document.querySelector('#menuCloseBtn')) {
+    const menuCloseBtn = document.querySelector('#menuCloseBtn');
+    const navMenu = document.querySelector('.navmenu')
+
+    menuCloseBtn.addEventListener('click', function (e) {
+        navMenu.classList.remove('navmenu--open');
+    });
+}
+
+if(document.querySelector('.menu-switcher')) {
+    const menuSwitch = document.querySelector('.menu-switcher');
+    const navMenu = document.querySelector('.navmenu');
+
+    menuSwitch.addEventListener('click', function(e) {
+        navMenu.classList.add('navmenu--open');
+    });
+}
