c4deb951890af829c63089ce39c6c28fc6c37f76
Fabian <19959@ma-web.nl>
2021-05-17 20:59:09 +0200

    site weggeschreven voor online plaatsing

---
+++ D1-K1-W2/code/dist/assets/product.dfac9714.js
@@ -0,0 +1,1 @@
+/* empty css              */import"./app.4617d5f6.js";import"https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js";
--- D1-K1-W2/code/vite.config.js
+++ D1-K1-W2/code/vite.config.js
@@ -11,6 +11,7 @@ module.exports = {
                 productOverview: resolve(__dirname, 'product-overview.html'),
                 product: resolve(__dirname, 'product.html'),
                 shoppingCart: resolve(__dirname, 'shopping-cart.html'),
+                order: resolve(__dirname, 'order.html'),
                 confirmPage: resolve(__dirname, 'confirmation.html') 
             }
         }
